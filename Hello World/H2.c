3ae56196be0b51464144fe2abdb24f6dc90b6bdc
Reyansh kumar <93653910+Reyanshkumar1111@users.noreply.github.com>
2022-04-28 23:55:29 +0530

    Add files via upload

---
+++ Hello World/H2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    char s[25];
+    scanf("%[^\n]s",s); // This will help to print till \n does not occur otherwise it will terminate when space will come.
+    printf("%s",s);
+
+    return 0;
+}
---
+++ Hello World/H5.c
@@ -0,0 +1,6 @@
+int main()
+{
+    printf("Hello World 11");
+    return 0;
+}
+// #include<stdio.h> is not necessary for the printf and scanf function.
