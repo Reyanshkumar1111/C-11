f7dc043e99207aa8e54c5d5976311e6065d029f9
Reyansh kumar <93653910+Reyanshkumar1111@users.noreply.github.com>
2022-04-29 22:14:19 +0530

    Update H5.c

--- Hello World/H5.c
+++ Hello World/H5.c
@@ -4,3 +4,15 @@ int main()
     return 0;
 }
 // #include<stdio.h> is not necessary for the printf and scanf function.
+
+//#include<stdio.h>
+
+int main()
+{
+    char a[25];
+    printf(" Enter your string : ");
+    scanf("%[^\n]s",a);
+    printf("                     %s",a);
+   
+    return 0;
+}
